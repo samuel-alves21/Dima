1af4a56964e3e33ef7507aec4229cc102c966463
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2024-09-11 17:55:41 -0300

    adding routes

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/products/", () => "Hello World!");
+app.MapGet("/products/{id}", () => "Hello World!");
+app.MapGet("/products/{id}/categories", () => "Hello World!");
+app.MapGet("/products/{id}/categories/{cid}/sub-categories", () => "Hello World!");
+
+
+app.MapPost("/", () => "Hello World!");
+app.MapPut("/", () => "Hello World!");
+app.MapDelete("/", () => "Hello World!");
 
 app.Run();
