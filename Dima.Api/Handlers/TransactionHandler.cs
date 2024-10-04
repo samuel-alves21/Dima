ea486186fd067c39a22632f1a3f6d745715e829b
Samuel Alves <samueltheodoro21@gmail.com>
2024-10-04 06:05:21 -0300

    update

--- Dima.Api/Handlers/TransactionHandler.cs
+++ Dima.Api/Handlers/TransactionHandler.cs
@@ -1,9 +1,9 @@
 ﻿using Dima.Api.Data;
+using Dima.Core.Common.Extensions;
 using Dima.Core.handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Transactions;
 using Dima.Core.Responses;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dima.Api.Handlers;
@@ -33,7 +33,6 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
         catch
         {
             return new Response<Transaction?>(null, 500, "Não foi possível criar a transação");
-
         }
     }
 
@@ -83,12 +82,55 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
 
     public async Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var transaction = await context.Transactions.FirstOrDefaultAsync((x) => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (transaction is null) return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+            return new Response<Transaction?>(transaction);
+        }
+        catch
+        {
+            return new Response<Transaction?>(null, 500, "Não foi possível atualizar a transação");
+        }
     }
 
     public async Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionByPeriodRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            request.StartDate ??= request.StartDate = DateTime.Now.GetFirstDay(); 
+            request.EndDate ??= DateTime.Now.GetLastDay();
+        }
+        catch
+        {
+            return new PagedResponse<List<Transaction>?>(null, 500, "Não foi possível determinar a data de início ou termino");
+
+        }
+
+        try
+        {
+            var query = context.Transactions
+                .AsNoTracking()
+                .Where((x) => x.UserId == request.UserId &&
+                        x.CreatedAt >= request.StartDate &&
+                        x.CreatedAt <= request.EndDate)
+                .OrderBy((x) => x.CreatedAt);
+
+            var transactions = await query
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .ToListAsync();
+
+            var count = await query.CountAsync();
+
+            return new PagedResponse<List<Transaction>?>(transactions, count, request.PageNumber, request.PageSize);
+        }
+        catch
+        {
+            return new PagedResponse<List<Transaction>?>(null, 500, "Não foi possível obter as transações");
+        }
     }
 }
 
