44eea109362d70f081eca16c788408b673f04d50
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2024-10-07 06:53:36 -0300

    update

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -1,11 +1,24 @@
-﻿using Dima.Core.Models;
+﻿using Dima.Api.Models;
+using Dima.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Dima.Api.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : 
+    IdentityDbContext<
+        User, 
+        IdentityRole<long>, 
+        long,
+        IdentityUserClaim<long>,
+        IdentityUserRole<long>,
+        IdentityUserLogin<long>,
+        IdentityRoleClaim<long>,
+        IdentityUserToken<long>
+    >
+    (options)
 {
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
